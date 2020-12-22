455ea2c03d95d45a26fff62894bedeabb577c892
j75689 <j75689@gmail.com>
2020-12-22 15:26:06 +0800

    use mock for redis_stream bench

--- pkg/mq/redis_stream/redis_stream_test.go
+++ pkg/mq/redis_stream/redis_stream_test.go
@@ -1,34 +1,62 @@
 package stream
 
 import (
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 
-	"github.com/rs/zerolog/log"
+	"github.com/adjust/rmq/v3"
+	"github.com/alicebob/miniredis"
+	"github.com/go-redis/redis/v7"
 	"github.com/j75689/Tmaster/pkg/config"
 	"github.com/j75689/Tmaster/pkg/mq"
+	"github.com/rs/zerolog/log"
 )
 
-func initLocalRedisMQ() (mq.MQ, error) {
-	return NewRedisStream(
-		config.MQConfig{
-			Driver: "redis_stream",
+func newMockRedisStream() (mq.MQ, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+
+	mr, err := miniredis.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+
+	_, err = client.Ping().Result()
+	if err != nil {
+		return nil, err
+	}
+	conn, err := rmq.OpenConnectionWithRedisClient(hostname+strconv.Itoa(rand.Int()), client, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &RedisStream{
+		config: config.MQConfig{
 			RedisStream: config.RedisStreamArg{
-				RedisOption: config.RedisConfig{
-					Host:         "localhost",
-					Port:         6379,
-					DB:           1,
-					PoolSize:     1000,
-					MinIdleConns: 100,
-				},
+				NumConsumers:   100,
+				PrefetchLimit:  100,
+				PollDuration:   time.Millisecond,
+				ProcessTimeout: 1 * time.Second,
 			},
 		},
-		log.Logger,
-	)
+		logger:     log.Logger,
+		conn:       conn,
+		publishers: &sync.Map{},
+		consumers:  &sync.Map{},
+	}, nil
 }
 
 func BenchmarkPublish(b *testing.B) {
-	mq, err := initLocalRedisMQ()
+	mq, err := newMockRedisStream()
 	if err != nil {
 		b.Error(err)
 		return
