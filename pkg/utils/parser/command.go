67a9a9437dc77a6b6bc819f2b2b7a136f7031fa8
j75689 <j75689@gmail.com>
2021-02-18 16:00:22 +0800

    extended function of pipe replace

--- pkg/utils/parser/command.go
+++ pkg/utils/parser/command.go
@@ -2,9 +2,11 @@ package parser
 
 import (
 	"bytes"
+	b64 "encoding/base64"
 	"encoding/json"
 	"errors"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -14,37 +16,110 @@ type (
 )
 
 const (
-	_Replace _CommandName = "replace"
+	_Replace      _CommandName = "replace"
+	_Base64Encode _CommandName = "base64encode"
+	_Base64Decode _CommandName = "base64decode"
+	_Quote        _CommandName = "quote"
+	_UnQuote      _CommandName = "unquote"
+)
+
+var (
+	_ReplaceCommand = func(pipeValue interface{}, args ...string) (interface{}, error) {
+		if len(args) < 1 || len(args) > 2 {
+			return nil, errInvalidArgument
+		}
+
+		oriValue := args[0]
+		newValue := ""
+		if len(args) == 2 {
+			newValue = args[1]
+		}
+
+		switch pipeValue.(type) {
+		case string:
+			return strings.Replace(pipeValue.(string), oriValue, newValue, -1), nil
+		case map[string]interface{}:
+			value, err := json.Marshal(pipeValue)
+			if err != nil {
+				return nil, errInvalidArgument
+			}
+			value = bytes.Replace(value, []byte(oriValue), []byte(newValue), -1)
+			result := map[string]interface{}{}
+			err = json.Unmarshal(value, &result)
+			if err != nil {
+				return nil, errInvalidArgument
+			}
+			return result, nil
+		case nil:
+			return nil, nil
+		}
+
+		return nil, errInvalidArgument
+	}
+
+	_Base64EncodeCommand = func(pipeValue interface{}, args ...string) (interface{}, error) {
+		switch pipeValue.(type) {
+		case string:
+			return b64.StdEncoding.EncodeToString([]byte(pipeValue.(string))), nil
+		case map[string]interface{}:
+			value, err := json.Marshal(pipeValue)
+			if err != nil {
+				return nil, errInvalidArgument
+			}
+			return b64.StdEncoding.EncodeToString(value), nil
+		case nil:
+			return nil, nil
+		}
+		return nil, errInvalidArgument
+	}
+
+	_Base64DecodeCommand = func(pipeValue interface{}, args ...string) (interface{}, error) {
+		switch pipeValue.(type) {
+		case string:
+			decodeStr, err := b64.StdEncoding.DecodeString(pipeValue.(string))
+			if err != nil {
+				return nil, errInvalidArgument
+			}
+			return string(decodeStr), nil
+		case nil:
+			return nil, nil
+		}
+		return nil, errInvalidArgument
+	}
+
+	_QuoteCommand = func(pipeValue interface{}, args ...string) (interface{}, error) {
+		switch pipeValue.(type) {
+		case string:
+			v := strconv.Quote(pipeValue.(string))
+			return v[1 : len(v)-1], nil
+		case nil:
+			return nil, nil
+		}
+		return nil, errInvalidArgument
+	}
+
+	_UnQuoteCommand = func(pipeValue interface{}, args ...string) (interface{}, error) {
+		switch pipeValue.(type) {
+		case string:
+			v, err := strconv.Unquote(pipeValue.(string))
+			if err != nil {
+				return nil, errInvalidArgument
+			}
+			return v, nil
+		case nil:
+			return nil, nil
+		}
+		return nil, errInvalidArgument
+	}
 )
 
 var (
 	commands = map[_CommandName]func(pipeValue interface{}, args ...string) (interface{}, error){
-		_Replace: func(pipeValue interface{}, args ...string) (interface{}, error) {
-			if len(args) != 2 {
-				return nil, errInvalidArgument
-			}
-
-			switch pipeValue.(type) {
-			case string:
-				return strings.Replace(pipeValue.(string), args[0], args[1], -1), nil
-			case map[string]interface{}:
-				value, err := json.Marshal(pipeValue)
-				if err != nil {
-					return nil, errInvalidArgument
-				}
-				value = bytes.Replace(value, []byte(args[0]), []byte(args[1]), -1)
-				result := map[string]interface{}{}
-				err = json.Unmarshal(value, &result)
-				if err != nil {
-					return nil, errInvalidArgument
-				}
-				return result, nil
-			case nil:
-				return nil, nil
-			}
-
-			return nil, errInvalidArgument
-		},
+		_Replace:      _ReplaceCommand,
+		_Base64Encode: _Base64EncodeCommand,
+		_Base64Decode: _Base64DecodeCommand,
+		_Quote:        _QuoteCommand,
+		_UnQuote:      _UnQuoteCommand,
 	}
 )
 
@@ -59,8 +134,7 @@ func ParseCmd(key string) (keys []string) {
 	for _, key := range temp {
 		if strings.HasPrefix(key, `'`) && strings.HasSuffix(key, `'`) {
 			key = key[1 : len(key)-1]
-		}
-		if strings.HasPrefix(key, `"`) && strings.HasSuffix(key, `"`) {
+		} else if strings.HasPrefix(key, `"`) && strings.HasSuffix(key, `"`) {
 			key = key[1 : len(key)-1]
 		}
 		keys = append(keys, key)
